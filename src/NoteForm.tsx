4608e8dea84b50e94851cba1044627d13867b7d0
Naveen Kolambage <naveenkolambage@Naveens-Laptop.hub>
2023-09-16 15:55:25 +1200

    added tags on listpage

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -26,7 +26,7 @@ export function NoteForm({onSubmit,onAddTag,availableTags}:NoteFormProps){
     const titleRef = useRef<HTMLInputElement>(null)
     const markdownRef = useRef<HTMLTextAreaElement>(null)
     const [selectedTags,setSelectedTags] = useState<Tag[] >([])
-
+    const navigate = useNavigate()
 
     function handleSubmit(e:FormEvent){
         e.preventDefault();
@@ -36,7 +36,7 @@ export function NoteForm({onSubmit,onAddTag,availableTags}:NoteFormProps){
             tags:selectedTags
         })
 
-//    navigate("..")
+  navigate("..")
     
     }
 
